25005551d0398eada9fb4d455b661ffa0b042996
Darian Anthony Patrick <dap@darianpatrick.com>
2011-08-19 23:01:54 -0400

    Encapsulate mcast listening in sub

--- bin/nsync.pl
+++ bin/nsync.pl
@@ -6,15 +6,29 @@ use warnings;
 use IO::Socket;
 use IO::Socket::Multicast;
 
+sub listen_for_nanostudio {
+	my $sock = IO::Socket::Multicast->new(
+		LocalPort => 10245,
+	);
+	$sock->mcast_add('225.1.1.1') || die "Couldn't set group: $!\n";
 
-my $sock = IO::Socket::Multicast->new(
-	LocalPort => 10245,
-);
-$sock->mcast_add('225.1.1.1') || die "Couldn't set group: $!\n";
+	while (1) {
+		my ($data, $peer);
 
-while (1) {
-	my ($data, $peer);
-	next unless $peer = $sock->recv($data, 10240);
-	next unless $data =~ m/^(.*):FileServer:(\d+);$/;
-	print $data, "\n";
+		next unless $peer = $sock->recv($data, 10240);
+		next unless $data =~ m/^(.*):FileServer:(\d+);$/;
+
+		my ($peer_port, $peer_addr) = sockaddr_in($peer);
+		$peer_addr = inet_ntoa($peer_addr);
+
+		return {addr => $peer_addr, port => $2};
+	}
 }
+
+sub main {
+	my $peer_info = listen_for_nanostudio();
+	print "$peer_info->{'addr'}:$peer_info->{'port'}\n";
+}
+
+main() if $0 eq __FILE__;
+
